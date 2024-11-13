93c1326741b6a2739572a455ec51be0707a36fc7
PracticeBetter <practice_practice25@yahoo.com>
2024-11-13 11:07:07 -0500

    added handleSubmit and Modified firebase utils to accomodate additional info in userAuth object

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -2,6 +2,8 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -10,8 +12,8 @@ const SignIn = () => {
 
   return (
     <div>
-      <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
