3bd49ee3c609182ec3dc23df25e12641ac74dbaf
PracticeBetter <practice_practice25@yahoo.com>
2024-11-17 11:19:33 -0500

    added only console.log userDocRef, will create new test branch after this

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -8,6 +8,7 @@ const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(userDocRef);
   };
 
   return (
