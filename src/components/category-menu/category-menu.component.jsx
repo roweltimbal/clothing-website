af93c6cda5f00cb89df6483b201f12088c9dc39b
PracticeBetter <practice_practice25@yahoo.com>
2025-02-01 20:48:26 -0500

    added category styles, also fixed scss styling clash

--- src/components/category-menu/category-menu.component.jsx
+++ src/components/category-menu/category-menu.component.jsx
@@ -1,11 +1,11 @@
-import CategoryItem from "../category-item/category-item.component";
+import DirectoryItem from "../directory-item/directory-item.component";
 import "./categories.styles.scss";
 
 const CategoryMenu = ({ categories }) => {
   return (
     <div className="categories-container">
       {categories.map((category) => {
-        return <CategoryItem key={category.id} category={category} />;
+        return <DirectoryItem key={category.id} category={category} />;
       })}
     </div>
   );
